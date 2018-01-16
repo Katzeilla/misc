0bd09c8d1f4055972735879cc7495f3f43c39499
Katzeilla <hello@katzeilla.org>
2018-01-16 08:33:22 -0500

    Add Variable

---
+++ JAVA/Variable/Variable.java
@@ -0,0 +1,16 @@
+class Variable {
+  public static void main(String[] args) {
+    int tops;
+    float gpa;
+    char key = 'A';
+    String name = "Variable";
+    byte first = 1;
+    short portNumber = 22;
+    long money = 123_000;
+    
+    int test = 5;
+    int test0 = 100;
+    int var = test * test0 + test++;
+    System.out.println(var);
+  }
+}
