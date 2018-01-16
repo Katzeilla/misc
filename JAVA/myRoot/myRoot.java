221fb189c1cf3c6ceac655b00bebf703160df062
Katzeilla <hello@katzeilla.org>
2018-01-16 08:22:34 -0500

    Add myROot

---
+++ JAVA/myRoot/myRoot.java
@@ -0,0 +1,10 @@
+class myRoot {
+  public static void main(String[] arguments) {
+    int number = Integer.parseInt(arguments[0]);
+    System.out.println("The square root of "
+        + number
+        + " is "
+        + Math.sqrt(number)
+        );
+  }
+}
