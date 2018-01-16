ab306ebce88646abab2a46233bdc59c3d47c7106
Katzeilla <hello@katzeilla.org>
2018-01-16 08:34:16 -0500

    Add PlanetWeight

---
+++ JAVA/PlanetWeight/PlanetWeight.java
@@ -0,0 +1,20 @@
+class PlanetWeight {
+  public static void main(String[] args) {
+    System.out.print("Your weigth on Earth is ");
+    double weight = 205;
+    System.out.println(weight);
+
+    System.out.print("Your weigth on Marcury is ");
+    double mercury = weight * .378;
+    System.out.println(mercury);
+
+    System.out.print("Your weigth on Moon is ");
+    double moon = weight * .166;
+    System.out.println(moon);
+
+    System.out.print("Your weigth on Jupiter is ");
+    double jupiter = weight * 2.364;
+    System.out.println(jupiter);
+
+  }
+}
